e1d53ffaae41f725dc276aae942ac06a2caaea73
UserTarun <saikalyantarun2001@gmail.com>
2022-12-12 00:35:27 +0530

    added some comments

--- main.js
+++ main.js
@@ -1,112 +1,156 @@
 
 
+const dict = {
+   squarecut: "/assets/squarecut.jpeg",
+   straightdrive: "/assets/straightdrive.jpeg",
+   ondrive: "/assets/ondrive.jpeg",
+   pull: "/assets/pull.jpeg",
+   legglance: "/assets/legglance.jpeg",
+   latecut: "/assets/latecut.jpeg",
+   flick: "/assets/flick.jpeg",
+   sweep: "/assets/sweep.jpeg",
+   uppercut: "/assets/uppercut.jpeg",
+   switchhit: "/assets/switchhit.jpeg",
+   hook: "/assets/hook.jpeg",
+   slog: "/assets/slog.jpeg",
+   coverdrive: "/assets/coverdrive.jpeg",
+   offdrive: "/assets/offdrive.jpeg",
+   reversesweep: "/assets/reversesweep.jpeg",
+   slogsweep: "/assets/slogsweep.jpeg",
+   cut: "/assets/cut.jpeg",
+   scoop: "/assets/scoop.jpeg",
+   squaredrive: "/assets/squaredrive.jpeg",
 
+   bouncer: "/assets/bouncer.jpeg",
+   inswinger: "/assets/inswinger.jpeg",
+   outswinger: "/assets/outswinger.jpeg",
+   legcutter: "/assets/legcutter.jpeg",
+   offcutter: "/assets/offcutter.jpeg",
+   reverseswing: "/assets/reverseswing.jpeg",
+   yorker: "/assets/yorker.jpeg",
+   pace: "/assets/pace.jpeg"
+}
 
 const strategies  = {
     squarecut :{
-       image : "/assets/squarecut.png",
+       image : "/assets/offcutter.jpeg",
        bestagainst :{ 
-           pace : "./assets/pace.png",
-           outswinger: "./assets/outswinger.png",
-           legcutter : "./assets/legcutter.png"
+           pace : "/assets/offcutter.jpeg",
+           outswinger: "/assets/offcutter.jpeg",
+           legcutter : "/assets/offcutter.jpeg"
     },
        badagainst:{
-           bouncer : "./assets/bouncer.png",
-           inswinger :"./assets/inswinger.png",
-           yorker :"./assets/yorker.png"
+           bouncer : "/assets/offcutter.jpeg",
+           inswinger :"/assets/offcutter.jpeg",
+           yorker :"/assets/offcutter.jpeg"
 
    }
     },
     pull: {
-       image : " ",
+       image : "/assets/offcutter.jpeg",
        bestagainst :{ 
            offcutter:"./assets/offcutter.png",
-           bouncer:"./assets/bouncer.png"
+           bouncer:"/assets/offcutter.jpeg"
     },
     badagainst:{
-       reverseswing:"./assets/reverseswing.png",
-       yorker:"./assets/yorker.png"
+       reverseswing:"/assets/offcutter.jpeg",
+       yorker:"/assets/offcutter.jpeg"
         
   }
    } ,
    latecut: {
-     image :"",
+     image :"/assets/offcutter.jpeg",
      bestagainst :{ 
         legcutter :"./assets/offcutter.png",
-        reversestring :"./assets/bouncer.png"
+        reversestring :"/assets/offcutter.jpeg"
  },
  badagainst:{
-   inswinger:"./assets/reverseswing.png",
-  yorker:"./assets/yorker.png"
+   inswinger:"/assets/offcutter.jpeg",
+  yorker:"/assets/offcutter.jpeg"
    }
    },
    flick : {
-     image: "",
+     image: "/assets/offcutter.jpeg",
      bestagainst :{ 
         inswinger :"./assets/offcutter.png",
-        offcutter :"./assets/bouncer.png"
+        offcutter :"/assets/offcutter.jpeg"
  },
  badagainst:{
-   bouncer:"./assets/reverseswing.png",
-  yorker:"./assets/yorker.png"
+   bouncer:"/assets/offcutter.jpeg",
+  yorker:"/assets/offcutter.jpeg"
    }
    }
   }
-
 
 
 
 
 function displayStratagies(){
+   //This loop iterates over strategies by shot names
   for(const shot in strategies){
+   //creating a container element i.e., Row container. 
+   //This container would contain three parts: left, middle, and right.
    let item = document.createElement("li");
    const itemClass = item.classList;
    itemClass.add("container");
 
-
+   //creating the left div
    let leftSec = document.createElement("div");
    const leftSecClass = leftSec.classList;
    leftSecClass.add("left");
 
-   for(const ball in strategies.shot.bestagainst){
+   //This loop iterates over the bestagainst object which has atmost three key-value pairs in it.
+   //we will then append individual images onto the left div.
+   for(const ball in shot.bestagainst){
       let image = document.createElement("img")
       let imgClass = item.classList;
       imgClass.add("playImg");
-      image.src = strategies.shot.bestagainst.ball;
+      image.src = shot.bestagainst.ball;
       image.alt = ball;
       leftSec.appendChild(image);
    }
-   item.appendChild("leftSec");
 
+   //appending the left div onto the item (container).
+   item.appendChild(leftSec);
 
+   ////creating the middle div
    let middleSec = document.createElement("div");
    const middleSecClass = middleSec.classList;
    leftSecClass.add("middle");
 
+   //Creating and appending the shot image onto the middle div
    let image = document.createElement("img")
    let imgClass = item.classList;
    imgClass.add("playImg");
-   image.src = strategies.shot.image;
+   image.src = shot.image;
    image.alt = shot;
    middleSec.appendChild(image);
+
+   //appending the middle div onto the item (container)
    item.appendChild(middleSec);
 
-
+   //creating the right div
    let rightSec = document.createElement("div");
    const rightSecClass = rightSec.classList;
    rightSecClass.add("right");
 
-   for(const ball in strategies.shot.badagainst){
+   //This loop iterates over the badagainst object which has atmost three key-value pairs in it.
+   //we will then append individual images onto the right div.
+   for(const ball in shot.badagainst){
       let image = document.createElement("img")
       let imgClass = item.classList;
       imgClass.add("playImg");
-      image.src = strategies.shot.badagainst.ball;
+      image.src = shot.badagainst.ball;
       image.alt = ball;
       rightSec.appendChild(image);
    }
 
+   //appending the right div onto the item (container)
    item.appendChild(rightSec);
   }
 
 
 }
+
+
+(() => displayStratagies())();
